2db82bf3fc9f9b516a3b2bc44aff7542077c25ae
ArpanaM <muriki.arpana@gmail.com>
2024-06-07 22:23:58 +0530

    Display Colors

--- examples/01-sample.sh
+++ examples/01-sample.sh
@@ -6,3 +6,12 @@ echo start nginx
 
 # Print colors
 
+echo -e "\e[31mHello In Red Color\e[0m"
+echo -e "\e[32mHello In Green Color\e[0m"
+echo -e "\e[33mHello In Yellow Color\e[0m"
+echo -e "\e[34mHello In Blue Color\e[0m"
+echo -e "\e[35mHello In Magenta Color\e[0m"
+echo -e "\e[36mHello In Cyan Color\e[0m"
+
+
+
