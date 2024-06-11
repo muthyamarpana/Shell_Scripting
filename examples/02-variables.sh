df8b6587bd5a09d829cad482824b43da2fe94281
ArpanaM <muriki.arpana@gmail.com>
2024-06-11 21:52:08 +0530

    Array

--- examples/02-variables.sh
+++ examples/02-variables.sh
@@ -14,3 +14,10 @@ read -p 'enter your name:' name
 read -p 'enter your age:' age
 
 echo "Your name = ${name}, Your age = ${age}"
+
+y=(10 20 30 40 50)
+echo ${y[0]}
+echo ${y[1]}
+echo ${y[2]}
+echo ${y[3]}
+echo ${y[4]}
